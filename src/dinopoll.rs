9e41077d02e0c6811bd18ece0a008575f37b2df8
Matthew Gleich <git@mattglei.ch>
2021-05-02 20:34:36 -0400

    feat: only include thank you message if I didn't create poll
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/dinopoll.rs
+++ src/dinopoll.rs
@@ -8,14 +8,23 @@ use serde_json::json;
 use crate::airtable::Poll;
 
 pub fn create_poll(client: &Client, poll: &Poll) -> Result<(), anyhow::Error> {
+	// Creating title based off author
+	let author_note = if poll.author != "UGTQ393RR" {
+		// If the user is not Matthew Gleich
+		format!(" -- thanks for the submission <@{}>!", &poll.question)
+	} else {
+		String::new()
+	};
+	let title = format!("{}{}", poll.question, author_note);
+
 	let response = client
 		.post("https://dinopoll.host.calebdenio.me/create")
 		.json(
-			&json!({"title": format!("{} (<@{}>)", poll.question, poll.author), "channel": env::var("DINOPOLL_CHANNEL")?, "options": poll.options, "othersCanAdd": poll.add}),
+			&json!({"title": title, "channel": env::var("DINOPOLL_CHANNEL")?, "options": poll.options, "othersCanAdd": poll.add}),
 		)
 		.bearer_auth(env::var("DINOPOLL_TOKEN")?)
 		.send()
-		.with_context(|| format!("Failed to create poll with question of {}", poll.question))?;
+		.with_context(|| format!("Failed to create poll with title of {}", title))?;
 
 	anyhow::ensure!(
 		response.status() == StatusCode::OK,
