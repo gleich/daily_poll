35def99d8e75738cbb331ef5b18de290f5c46703
Matthew Gleich <git@mattglei.ch>
2021-05-15 21:35:39 -0400

    feat: clean up custom scheduler
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,39 @@ mod dinopoll;
 fn main() {
 	tracing_subscriber::fmt::init();
 
-	// Creating client
 	let client = Client::new();
 	info!("Created client");
 
-	let sleep_time = Duration::from_secs(60);
+	let sleep_time = Duration::from_millis(50);
 	loop {
 		let now = Utc::now();
-		if now.hour() == 12 && now.minute() == 0 {
-			println!("\n");
-			info!("Cycle engaged");
 
-			let poll_data =
-				airtable::get_polls(&client).expect("Failed to get polls from airtable");
-			info!("Got polls from airtable");
-
-			for poll in poll_data {
-				if !poll.used {
-					dinopoll::create_poll(&client, &poll)
-						.expect("Failed to create poll with dinopoll");
-					info!("Posted poll with question of \"{}\"", poll.question);
-					airtable::set_as_used(&client, &poll).expect("Failed to set poll to used");
-					info!(
-						"Set poll of question \"{}\" to used in airtable",
-						poll.question
-					);
-					break;
-				}
-			}
+		match (now.hour(), now.minute()) {
+			(12, 0) => post_poll(&client),
+			_ => (),
 		}
 
 		thread::sleep(sleep_time);
+	}
+}
+
+fn post_poll(client: &Client) {
+	println!("\n");
+	info!("Cycle engaged");
+
+	let poll_data = airtable::get_polls(client).expect("Failed to get polls from airtable");
+	info!("Got polls from airtable");
+
+	for poll in poll_data {
+		if !poll.used {
+			dinopoll::create_poll(client, &poll).expect("Failed to create poll with dinopoll");
+			info!("Posted poll with question of \"{}\"", poll.question);
+			airtable::set_as_used(client, &poll).expect("Failed to set poll to used");
+			info!(
+				"Set poll of question \"{}\" to used in airtable",
+				poll.question
+			);
+			break;
+		}
 	}
 }
