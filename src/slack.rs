dc385c5134c34f7cae6df87e0bb39a9bda47c5be
Matthew Gleich <git@mattglei.ch>
2021-05-25 13:51:46 -0400

    feat: add gadzooks message if under 2 in quene
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- src/slack.rs
+++ src/slack.rs
@@ -10,20 +10,28 @@ use crate::airtable::{self, Poll};
 pub const MATT_GLEICH_SLACK_ID: &str = "UGTQ393RR";
 
 pub fn send_reminder(client: &Client) -> Result<()> {
+	let polls_left = airtable::get_polls(client)?
+		.into_iter()
+		.filter(|p| !p.used)
+		.collect::<Vec<Poll>>()
+		.len();
+
 	let response = client
 		.post(env::var("SLACK_WEBHOOK_URL").context("Failed to find slack webhook URL env var")?)
 		.json(&json!({
 			"text":
 				format!(
 					"Hey everyone! Just a little reminder that you can DM <@{}> if you have an \
-					 idea for a poll. We currently have {} polls in the queue. Have a good day \
-					 everyone :)",
+					 idea for a poll. {} Have a good day everyone :)",
 					MATT_GLEICH_SLACK_ID,
-					airtable::get_polls(client)?
-						.into_iter()
-						.filter(|p| !p.used)
-						.collect::<Vec<Poll>>()
-						.len()
+					if polls_left <= 2 {
+						format!(
+							"Gadˈzo͞oks we only have {} polls left in the queue!!",
+							polls_left
+						)
+					} else {
+						format!("We currently have {} polls left in the quene.", polls_left)
+					}
 				)
 		}))
 		.send()
