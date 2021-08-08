2932d09e560ef2762928543e480c4bca87394782
Matt Gleich <git@mattglei.ch>
2021-08-08 01:32:55 -0400

    fix(lint): rustfmt schema.rs
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/schema.rs
+++ src/schema.rs
@@ -1,22 +1,19 @@
 table! {
-    poll_options (id) {
-        id -> Integer,
-        question -> Varchar,
-        option_name -> Varchar,
-    }
+	poll_options (id) {
+		id -> Integer,
+		question -> Varchar,
+		option_name -> Varchar,
+	}
 }
 
 table! {
-    polls (question) {
-        question -> Varchar,
-        author -> Varchar,
-        used -> Bool,
-        add_options -> Bool,
-        multiselect -> Bool,
-    }
+	polls (question) {
+		question -> Varchar,
+		author -> Varchar,
+		used -> Bool,
+		add_options -> Bool,
+		multiselect -> Bool,
+	}
 }
 
-allow_tables_to_appear_in_same_query!(
-    poll_options,
-    polls,
-);
+allow_tables_to_appear_in_same_query!(poll_options, polls,);
